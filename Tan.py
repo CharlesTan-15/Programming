cc9e798ccca5dc39f956e49027257dc57a9ffc4b
CharlesTan-15 <tancharlesjames@gmail.com>
2024-08-31 18:03:36 +0800

    Add files via upload

---
+++ Tan.py
@@ -0,0 +1,20 @@
+print("""
+  .oooooo.   oooo                           oooo                    
+ d8P'  `Y8b  `888                           `888                    
+888           888 .oo.    .oooo.   oooo d8b  888   .ooooo.   .oooo.o
+888           888P"Y88b  `P  )88b  `888""8P  888  d88' `88b d88(  "8
+888           888   888   .oP"888   888      888  888ooo888 `"Y88b. 
+`88b    ooo   888   888  d8(  888   888      888  888    .o o.  )88b
+ `Y8bood8P'  o888o o888o `Y888""8o d888b    o888o `Y8bod8P' 8""888P'
+                                                                    
+                                                                    
+                                                                    
+   oooo                                                             
+   `888                                                             
+    888  .oooo.   ooo. .oo.  .oo.    .ooooo.   .oooo.o              
+    888 `P  )88b  `888P"Y88bP"Y88b  d88' `88b d88(  "8              
+    888  .oP"888   888   888   888  888ooo888 `"Y88b.               
+    888 d8(  888   888   888   888  888    .o o.  )88b              
+.o. 88P `Y888""8o o888o o888o o888o `Y8bod8P' 8""888P'              
+`Y888P                                                              
+""")
